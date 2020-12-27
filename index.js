63e85ca07f8c3a3941441a50f658494f957546e3
Lukas Mo <67667042+adammolina@users.noreply.github.com>
2020-12-27 12:42:32 -0500

    added countdown to timerOne element

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 document.querySelector("button").addEventListener("click", function() {
 
-
-
-
-    alert("I was clicked!");
-
-
-
+    const startingMinutes = 0.1;
+    let time = startingMinutes * 60;
     
-});
+    const countDownOne = document.getElementById('timerOne');
+    
+    setInterval(updateCountdown, 1000);
+    
+    function updateCountdown() {
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+    
+        countDownOne.innerHTML = `${minutes}: ${seconds}`;
+        time--;
+    }
+    
+
+    });
+
+
