8b8742de3db69bb4ea42af53bb636e425250bd3b
Lukas Mo <67667042+adammolina@users.noreply.github.com>
2020-12-27 15:32:28 -0500

    idek

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 document.querySelector("button").addEventListener("click", function() {
 
-    //First Timer variables
-    const startingMinutes = 0.1;
+//First Timer variables
+    const startingMinutes = 0.2;
     let time = startingMinutes * 60;
     const countDownOne = document.getElementById('timerOne');
 
-    //Second Timer variables
+//Second Timer variables
     const startingMinutesTwo = 0.2;
     let timeTwo = startingMinutesTwo * 60;
     const countDownTwo = document.getElementById('timerTwo');
- 
-    //First Timer
-    
-setInterval(updateCountdown, 1000);//runs the code every second
+
+//First Timer
+    setInterval(updateCountdown, 1000);//runs the code every second
     function updateCountdown() {
         const minutes = Math.floor(time / 60);
         let seconds = time % 60;
-
-        seconds = seconds < 10 ? '0' + seconds : seconds;
     
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        
         countDownOne.innerHTML = `${minutes}: ${seconds}`;
         time !== 0 ? time-- : time;;
-        
-    }
+        }
 
 //Second Timer
-setTimeout(updateTimerTwo, 6000);
 
-function updateTimerTwo() {
+setTimeout(delayTimerTwo, 12000); //delays the code starting for the first 50 minutes
+
+    function delayTimerTwo() {
     setInterval(updateCountdownTwo, 1000); //runs the code every second
+
     function updateCountdownTwo() {
         const minutesTwo = Math.floor(timeTwo / 60);
         let secondsTwo = timeTwo % 60;
@@ -38,7 +38,7 @@ function updateTimerTwo() {
         countDownTwo.innerHTML = `${minutesTwo}: ${secondsTwo}`;
         timeTwo !== 0 ? timeTwo-- : timeTwo;;
         return outputTwo;
-}
+    }
 }
 
 });
